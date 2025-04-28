662dee660176931f2e48734b65557a40688f8feb
Artem Agasyan <artem@MacBook-Pro-Artem-no.local>
2025-04-28 13:40:44 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+export default function LuzhnikiBeach() {
+  return (
+    <div className="flex items-center justify-center h-screen text-3xl font-bold">
+      Здесь будет твой код из Canvas
+    </div>
+  );
+}
